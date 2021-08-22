4541e159f4401af2dcb6a30437cdd5c1a653df76
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-08-22 19:35:18 +0200

    Added ability to easily get data and test from app.py

--- edu/pwr/main/app.py
+++ edu/pwr/main/app.py
@@ -2,6 +2,15 @@ from edu.pwr.database.DataProcessing import *
 import psycopg2
 
 
+def getSensor(conn, sid):
+    with conn.cursor() as cursor:
+        query = 'SELECT * FROM dbo.Sensors WHERE sensorID = %s;'
+        data = [sid]
+        cursor.execute(query, data)
+        sensor_info = cursor.fetchone()
+        return Sensor(sensor_info[0], sensor_info[1], sensor_info[2], sensor_info[3], sensor_info[4], sensor_info[5], sensor_info[6], sensor_info[7])
+
+
 def main():
     print("connecting to server")
     conn = psycopg2.connect(
@@ -12,7 +21,9 @@ def main():
 
     start = datetime(2019, 12, 1, 0)
     end = datetime(2019, 12, 31, 23)
-    dataSummary(conn, start, end)
+    my_sensor = getSensor(conn, 11563)
+    my_sensor.getData(conn, start, end)
+    #dataSummary(conn, start, end)
     conn.close()
 
 
