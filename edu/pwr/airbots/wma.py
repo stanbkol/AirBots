6c3ff1b4d7166d26997e09a41748456b7bfee3c6
stanbkol <stanbkol@gmail.com>
2021-08-22 17:03:52 +0200

    wma file created

---
+++ edu/pwr/airbots/wma.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def weightedmovingaverage(Data, period):
+    weighted = []
+    for i in range(len(Data)):
+            try:
+                total = np.arange(1, period + 1, 1) # weight matrix
+                matrix = Data[i - period + 1: i + 1, 3:4]
+                matrix = np.ndarray.flatten(matrix)
+                matrix = total * matrix # multiplication
+                wma = (matrix.sum()) / (total.sum()) # WMA
+                weighted = np.append(weighted, wma) # add to array
+            except ValueError:
+                pass
+    return weighted
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
