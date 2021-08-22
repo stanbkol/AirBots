ea9d2568c95aa76161b8babe70fcf469c872dde3
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-08-22 17:21:37 +0200

    Small tweaks to use and location of sensor summary method

--- edu/pwr/main/app.py
+++ edu/pwr/main/app.py
@@ -1,19 +1,15 @@
 from edu.pwr.database.DataProcessing import *
-
 import psycopg2
 
 
 def main():
-    global invalid_count
     print("connecting to server")
     conn = psycopg2.connect(
         host="pgsql13.asds.nazwa.pl",
         database="asds_PWR",
         user="asds_PWR",
         password="W#4bvgBxDi$v6zB")
-
     dataSummary(conn)
-
     conn.close()
 
 
