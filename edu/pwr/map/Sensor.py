33e30e8b48c6a55fef59bc1a85ed728441a53085
stanbkol <stanbkol@gmail.com>
2021-09-12 16:02:58 +0200

    initial branch for adopting the ORM framework

--- edu/pwr/map/Sensor.py
+++ edu/pwr/map/Sensor.py
@@ -2,15 +2,17 @@ from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from edu.pwr.database.DbManager import createEngine
+from edu.pwr.map.MapPoint import calcDistance, MapPoint
 
 base = declarative_base()
 
 
 class Sensor(base):
     __tablename__ = 'sensors'
+    __table_args__ = {"schema": "airbots"}
 
     sid = Column('sensor_id', Integer, primary_key=True)
-    tid = Column('tile_id', Integer, ForeignKey("user.user_id"), nullable=False)
+    tid = Column('tile_id', Integer, ForeignKey("tile.tile_id"), nullable=False)
     adr1 = Column('address1', String(50))
     adr2 = Column('address2', String(50))
     adrn = Column('address_num', String(5))
@@ -35,5 +37,18 @@ class Sensor(base):
         return "<Sensor(sensorid=%s,tileid=%s, lat=%s, lon=%s, elev=%s)>" % (self.sid, self.tid, self.lat, self.lon,
                                                                              self.elv)
 
+    def setAgent(self, a):
+        self.agent = a
+
+    def changeState(self, s):
+        self.state = s
+
     def __str__(self):
-        return "Sensor_%s latlon,elv: (%s, %s, %s)" % (self.sid, self.lat, self.lon, self.elv)
+        return "Sensor_" + str(self.sensorid) + " Address Line 1='" + str(self.address1) + "' Address Line 2='" + str(self.address2) + \
+               "' Address Number='" + str(self.addressnumber) + "' lat,lon,elev=(" + str(self.latitude) + ", " + \
+               str(self.longitude) + ", " + str(self.elevation) + ")"
+
+    def metersTo(self, other):
+        if isinstance(other, Sensor):
+            return calcDistance(startLL=MapPoint(self.latitude, self.longitude),
+                                endLL=MapPoint(other.latitude, other.longitude))
