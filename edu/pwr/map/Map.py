3be276e8ad351df9ff037b09602f0ccd03fedaca
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-09-13 16:13:43 +0200

    Revert "Feat orm modeling matt"

--- edu/pwr/map/Map.py
+++ edu/pwr/map/Map.py
@@ -1,23 +1,8 @@
-from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-base = declarative_base()
-
-
 class Map:
-    __tablename__ = 'maps'
-    __table_args__ = {"schema": "airbots"}
 
     # collection of tiles-> collection tiles with coords and elevation.
     tileMesh = []
     aggregationOptions = []
-
-    map_ID = Column('map_id', Integer,  primary_key=True)
-    name = Column('name', String(20))
-    coord_NW = Column('C_NW', Float)
-    coord_NE = Column('C_NE', Float)
-    coord_SW = Column('C_SW', Float)
-    coord_SE = Column('C_SE', Float)
 
     def __init__(self, coord_NW, coord_NE, coord_SW, coord_SE, map_ID, name):
         self.coord_NW = coord_NW
