78e82be0d6503f618b6407c317040da81635e168
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-09-17 17:58:00 +0200

    Moved unnecessary methods into correct files. app.py will be used to run airbots files for testing agents

--- edu/pwr/airbots/wma.py
+++ edu/pwr/airbots/wma.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 from edu.pwr.database.utils import drange
 
@@ -67,6 +69,34 @@ def spatial_weights(sensor, n):
     sid_dist = {n[s].sid: (n[d]/sum_dist) for n in neighbors}
 
     return sid_dist
+
+
+def getPM1(conn, sid, start_interval=None, end_interval=None):
+    with conn.cursor() as cursor:
+        if not start_interval and not end_interval:
+            query = 'SELECT * FROM dbo.Measurements WHERE sensorID = %s;'
+            data = [sid]
+        else:
+            query = 'SELECT sensorid, date, pm1 FROM dbo.Measurements WHERE sensorID = %s AND date BETWEEN %s AND %s;'
+            data = [sid, start_interval, end_interval]
+        cursor.execute(query, data)
+        cols = []
+        for elt in cursor.description:
+            cols.append(elt[0])
+        data_list = cursor.fetchall()
+        return cols, data_list
+
+
+def show_wma(conn, start, end):
+    cols, measures = getPM1(conn, 11563, start, end)
+    conn.close()
+    res = weighted_rolling_mean(measures, 24, cols, exp=True)
+    plt.plot(res['date'], res['pm1'], label="PM1 Values")
+    plt.plot(res['date'], res['MA_PM1'], label="MA Values")
+    plt.xlabel('Dates')
+    plt.ylabel('Values')
+    plt.legend()
+    plt.show()
 
 def run():
     xp = exp_weights(2)
--- edu/pwr/main/app.py
+++ edu/pwr/main/app.py
@@ -1,69 +1,15 @@
-from edu.pwr.database.DataProcessing import *
-from edu.pwr.database.DbManager import insertSensors, insertMeasures, addOpoleMap
-import psycopg2
-import matplotlib.pyplot as plt
-from edu.pwr.airbots.wma import *
-from IPython.display import display
-from edu.pwr.database.DbManager import engine, Base, Session, insertTiles
-from edu.pwr.map.Map import Map
-from edu.pwr.map.Sensor import Sensor
-from edu.pwr.map.Measure import Measure
-from edu.pwr.map.Tile import Tile
-
-
-def getPM1(conn, sid, start_interval=None, end_interval=None):
-    with conn.cursor() as cursor:
-        if not start_interval and not end_interval:
-            query = 'SELECT * FROM dbo.Measurements WHERE sensorID = %s;'
-            data = [sid]
-        else:
-            query = 'SELECT sensorid, date, pm1 FROM dbo.Measurements WHERE sensorID = %s AND date BETWEEN %s AND %s;'
-            data = [sid, start_interval, end_interval]
-        cursor.execute(query, data)
-        cols = []
-        for elt in cursor.description:
-            cols.append(elt[0])
-        data_list = cursor.fetchall()
-        return cols, data_list
-
-
-def show_wma():
-    conn = createConnection()
-    start = datetime(2020, 1, 1, 0)
-    end = datetime(2020, 1, 8, 0)
-    cols, measures = getPM1(conn, 11563, start, end)
-    conn.close()
-    res = weighted_rolling_mean(measures, 24, cols, exp=True)
-    plt.plot(res['date'], res['pm1'], label="PM1 Values")
-    plt.plot(res['date'], res['MA_PM1'], label="MA Values")
-    plt.xlabel('Dates')
-    plt.ylabel('Values')
-    plt.legend()
-    plt.show()
-
-
-def populateTables(conn):
-    s_list = getSensors(conn, '*')
-    print("sensor data fetched")
-    m_list = getMeasures(conn, '*')
-    print("measurement data fetched")
-    tiles = getTiles(conn, '*')
-    print("tilebin data fetched")
-    conn.close()
-    print("inserting maps..")
-    addOpoleMap()
-    print("inserting tiles..")
-    insertTiles(tiles)
-    print("inserting sensors..")
-    insertSensors(s_list)
-    print("inserting measures..")
-    insertMeasures(m_list)
+from datetime import datetime
+from edu.pwr.airbots.wma import show_wma
+from edu.pwr.database.DataLoader import createConnection
 
 
 def main():
-    conn = createConnection()
+    start = datetime(2020, 1, 1, 0)
+    end = datetime(2020, 1, 8, 0)
+
+    #conn = createConnection()
+    #show_wma(conn, start, end)
 
 
 if __name__ == '__main__':
-    # sensors_test()
     main()
