dc090ec5fe35573c189204feb4b22f674ca59767
Khush Saraswat <khushsaraswat@gmail.com>
2021-09-17 16:56:11 +0200

    More variants and tests for moving average with skeleton for a distnace based moving average. Attempting to predict values bassed on distance.

---
+++ edu/pwr/airbots/wma4.py
@@ -0,0 +1,11 @@
+# weights based on distance for calculating moving average
+from edu.pwr.map.MapPoint import calcDistance, MapPoint
+
+A = MapPoint(1,1)
+B = MapPoint(2,2)
+
+def main():
+    print(calcDistance(A,B))
+
+if __name__ == '__main__':
+    main()
