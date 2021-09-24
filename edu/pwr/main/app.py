f74f4f9781a2a029e259d3e7debb9445d7aad601
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-09-24 22:07:25 +0200

    Updated final agent to work with new ORM functionality

--- edu/pwr/main/app.py
+++ edu/pwr/main/app.py
@@ -5,14 +5,11 @@ from edu.pwr.map.Agents import *
 
 def main():
     start = datetime(2020, 1, 1, 0)
-    end = datetime(2020, 5, 31, 0)
+    end = datetime(2020, 1, 2, 0)
     s1 = Sensor.getSensor(11563)
-    print(s1)
     measures = s1.getMeasures(start_interval=start, end_interval=end)
-    # my_agent = MovingAverageV2()
-    # my_agent.makePrediction(measures, 10)
-    my_agent_2 = MovingAverageV1()
-    my_agent_2.makePrediction(measures, 10)
+    my_agent_2 = MultiDimensionV1()
+    #my_agent_2.makePrediction(measures)
     # print(s1.nearestNeighbors(2))
 
 
