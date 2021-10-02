52b31c38fa678083bf49e39bb4ddcbf85aa5a744
wolfautopsy <75130868+wolfautopsy@users.noreply.github.com>
2021-10-02 02:20:15 +0200

    Finished methods for cleaning interval

--- edu/pwr/main/app.py
+++ edu/pwr/main/app.py
@@ -5,16 +5,15 @@ from edu.pwr.map.Model import *
 
 
 def main():
-    #modelSummary(getJson("C:\\Users\\User\\PycharmProjects\\AirBots\\Docs\\Model1"))
-
+    # modelSummary(getJson("C:\\Users\\User\\PycharmProjects\\AirBots\\Docs\\Model1"))
     start = datetime(2020, 1, 1, 0)
     end = datetime(2020, 1, 7, 0)
-    sensor = getSensorORM(11563)
+    sensor = getSensorORM(11640)
     data = sensor.getMeasures(start, end)
-    agent = MovingAverageV1()
-    print(agent.cf)
-    agent.makePrediction(data)
-    print(agent.cf)
+    sorted_data = sorted(data, key=lambda x: x.dk)
+    final_data = cleanInterval(sorted_data)
+    for m in final_data:
+        print(m)
 
 
 if __name__ == '__main__':
--- edu/pwr/map/Model.py
+++ edu/pwr/map/Model.py
@@ -2,6 +2,7 @@ from edu.pwr.map.Models import *
 from edu.pwr.map.Agents import *
 from dateutil import parser
 import json
+import pandas
 
 
 def makeModel(file):
@@ -19,11 +20,35 @@ def countInterval(start, end):
     diff = end-start
     days, seconds = diff.days, diff.seconds
     total_intervals = days * 24 + seconds // 3600
-    return total_intervals+1
+    return total_intervals
+
+
+def fillInterval(startMeasure, endMeasure):
+    avg_pm1 = (startMeasure.pm1 + endMeasure.pm1) / 2
+    avg_pm10 = (startMeasure.pm10 + endMeasure.pm10) / 2
+    avg_pm25 = (startMeasure.pm25 + endMeasure.pm25) / 2
+    avg_temp = (startMeasure.temp + endMeasure.temp) / 2
+    time_range = pandas.date_range(startMeasure.date, endMeasure.date, freq='H')
+    Soft_Data = []
+    for e in time_range:
+        dk = int(e.strftime('%Y%m%d%H'))
+        Soft_Data.append(
+            Measure(date_key=dk, sensor_id=startMeasure.sid, date=e, pm1=avg_pm1, pm10=avg_pm10, pm25=avg_pm25,
+                    temperature=avg_temp))
+    Soft_Data.pop(0)
+    Soft_Data.pop()
+    return Soft_Data
+
+
+def cleanInterval(data):
+    for i, x in zip(data, data[1:]):
+        if (countInterval(i.date, x.date)) != 1:
+            data.extend(fillInterval(i, x))
+    return sorted(data, key=lambda x: x.dk)
 
 
 def rateInterval(dataset, total):
-    return dataset/total
+    return (dataset-1)/total
 
 
 def modelSummary(model_data):
