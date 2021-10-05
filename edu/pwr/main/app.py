b5af68bd76b94a752140f4f4260c0f46989ed275
Rusiecki <matthew.rusiecki@nokia.com>
2021-10-05 10:44:44 +0200

    Finalized structure to run model and save results.

--- edu/pwr/main/app.py
+++ edu/pwr/main/app.py
@@ -1,19 +1,9 @@
 from datetime import datetime
-from edu.pwr.map.Models import *
-from edu.pwr.map.Agents import *
 from edu.pwr.map.Model import *
 
 
 def main():
-    # modelSummary(getJson("C:\\Users\\User\\PycharmProjects\\AirBots\\Docs\\Model1"))
-    start = datetime(2020, 1, 1, 0)
-    end = datetime(2020, 1, 7, 0)
-    sensor = getSensorORM(11640)
-    data = sensor.getMeasures(start, end)
-    sorted_data = sorted(data, key=lambda x: x.dk)
-    final_data = cleanInterval(sorted_data)
-    for m in final_data:
-        print(m)
+    Model(r"C:\Users\mrusieck\PycharmProjects\AirBot\Docs\Model1")
 
 
 if __name__ == '__main__':
