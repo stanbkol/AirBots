d67e68696616fa176b13f481859bed41eb6fe6c2
stanbkol <stanbkol@gmail.com>
2021-10-09 18:38:53 +0200

    fixing main repopulating tables

--- src/main/app.py
+++ src/main/app.py
@@ -1,9 +1,13 @@
 from src.map.Model import *
+from src.database.Models import *
+from src.database.DbManager import engine
 
 
 def main():
-    #Model(r"C:\Users\mrusieck\PycharmProjects\AirBot\Docs\Model1")
-    classifyTiles(r"C:\Users\User\Desktop\Multi-Agent\tile_info")
+    # Model(r"C:\Users\mrusieck\PycharmProjects\AirBot\Docs\Model1")
+    # classifyTiles(r"C:\Users\User\Desktop\Multi-Agent\tile_info")
+    createAllTables(engine)
+    populateTables()
 
 
 if __name__ == '__main__':
