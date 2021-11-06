c5405d0cb7f6cc325d8f82b70c3180220a743c43
stanbkol <stanbkol@gmail.com>
2021-11-06 20:39:30 +0100

    fixed model predicts, except randomModel. Agent now weighs predictions by squared error

--- src/agents/Agents.py
+++ src/agents/Agents.py
@@ -1,21 +1,22 @@
-from ForecastModels import RandomModel, NearbyAverage, MinMaxModel, CmaModel, MultiVariate
+from src.agents.ForecastModels import RandomModel, NearbyAverage, MinMaxModel, CmaModel, MultiVariate
 
 
 class Agent(object):
-    def __init__(self, sensor_id, thresholds, config=None, confidence=100):
+    def __init__(self, sensor_id, thresholds, sensor_list, config=None, confidence=100):
         self.sid = sensor_id
         self._threshold = thresholds
         self._configs = config
         self.confidence = confidence
+        self.sensors = sensor_list
         self.models = self._initializeModels()
         self.error = 0
 
     def _initializeModels(self):
-        models = {"rand": RandomModel(self.sid),
-                  'nearby': NearbyAverage(self.sid),
-                  'minmax': MinMaxModel(self.sid),
-                  'sma': CmaModel(self.sid),
-                  'mvr': MultiVariate(self.sid)
+        models = {"rand": RandomModel(self.sid, self.sensors),
+                  'nearby': NearbyAverage(self.sid, self.sensors),
+                  'minmax': MinMaxModel(self.sid, self.sensors),
+                  'sma': CmaModel(self.sid, self.sensors),
+                  'mvr': MultiVariate(self.sid, self.sensors)
                   }
 
         return models
@@ -23,12 +24,12 @@ class Agent(object):
     def _getModelNames(self):
         return ['rand', 'nearby', 'minmax', 'sma', 'mvr']
 
-    def _weightModels(self, actual):
+    def _weightModels(self, predicts, actual):
         errors = {}
         total_se = 0
         for model_name in self._getModelNames():
-            model_prediction = self.models[model_name].getPrediction()
-            squared_error = self._calc_squared_error(model_prediction, actual)
+            model_prediction = predicts[model_name]
+            squared_error = self._calc_squared_error(model_prediction['pm1'], actual)
             total_se += squared_error
             errors[model_name] = squared_error
 
@@ -40,9 +41,22 @@ class Agent(object):
     def _calc_squared_error(self, prediction, actual):
         return (actual-prediction)**2
 
-    def _makePrediction(self):
+    def makePredictions(self, target_sid, target_time, values, meas=None):
+        measure = meas
+        predicts = {}
         for model in self._getModelNames():
-            self.models[model].makePrediction(self.sid, )
+            if model == 'mvr':
+                predicts[model] = self.models[model].makePrediction(target_time, values, target_sid)
+            else:
+                predicts[model] = self.models[model].makePrediction(target_time, values)
+
+        weights = self._weightModels(predicts, getattr(measure, 'pm1'))
+        total_pm1 = 0
+        for model in weights.keys():
+            total_pm1 += weights[model] * predicts[model]['pm1']
+
+        print(f'Sensor %s, ')
+        return total_pm1
 
 
 if __name__ == '__main__':
