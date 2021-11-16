924455355c6cf4eacaf80aef984e48708c3bdf1f
stanbkol <stanbkol@gmail.com>
2021-11-16 17:38:48 +0100

    fixed some notebook errors. added same for main.

--- src/main/app.py
+++ src/main/app.py
@@ -8,6 +8,30 @@ from src.map.Central import *
 from src.map.HexGrid import geo_tiles_from_db, genSensorLayer_db, geojson_from_tiles
 
 
+def unique_tiles(tiles):
+    unique_tiles = {}
+
+    for t in tiles:
+        if t.tid not in unique_tiles.keys():
+            unique_tiles[t.tid] = t
+
+    return [unique_tiles[k] for k, v in unique_tiles.items()]
+
+
+def tile_ranges(sensor_tiles, r=5):
+    """
+    given list of tiles and range, returns a list of unique tiles that are in range of each
+    tile provided.
+    """
+    tiles = []
+    for t in sensor_tiles:
+        neighbors = t.tiles_in_range(r)  # this includes the center tile: t
+        tiles.extend(neighbors)
+        print(f"\tgot {len(neighbors)} neighbors for tile {t.tid}")
+
+    return unique_tiles(tiles)
+
+
 def main():
     # c = Central('..\\..\\..\\AirBots\\docs\\Model1')
     # data = getMeasuresORM(11571, datetime.datetime(2019, 1, 1, 0), datetime.datetime(2019, 1, 30, 0))
@@ -15,20 +39,21 @@ def main():
     #     print(e)
     # print(len(data))
     # c.sensorSummary()
-    # up_sensors_tids()
-    # sensorBoundGrid()
 
-    id  = 10756
-    tile = getTileORM(id)
-    r = 5
-    circle = tile.tiles_in_range(r)
-    print(len(circle))
+    tiles = getSensorTiles()
+    dist = 3
+    clusters = tile_ranges(tiles, r=dist)
+    print(len(clusters))
+    geojson_from_tiles(clusters, f"sensorTiles_{dist}_ranges.geojson")
     # geojson_from_tiles(circle, f"{id}_{r}_range.geojson")
     # genSensorLayer_db()
     # print("finished making sensor geojson")
     #
     # geo_tiles_from_db()
     # print("finished making tiles geojson")
+
+
+
 
 
 if __name__ == '__main__':
