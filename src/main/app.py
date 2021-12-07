b3a64ad2820f47bdeee09af71978fdcb05942273
Rusiecki <matthew.rusiecki@nokia.com>
2021-12-07 14:44:36 +0100

    Model File Updates

--- src/main/app.py
+++ src/main/app.py
@@ -2,12 +2,10 @@ from datetime import datetime
 
 from src.map.Central import *
 from src.database.Models import *
-from src.main.utils import tile_ranges
-from src.map.HexGrid import geojson_from_tiles, genSensorLayer_db
 
 
 def main():
-    c = Central('..\\..\\..\\AirBots\\docs\\Model2')
+    c = Central('..\\..\\..\\AirBots\\docs\\Model1')
     c.makePrediction(5697, datetime(2019, 1, 7, 0))
 
 
